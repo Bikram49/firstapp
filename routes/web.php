3b12680d5d794f532c243bd914f8248da997d3b7
Bikram Shrestha <bikramsoftnep@gmail.com>
2018-05-17 17:03:39 +0545

    till migration file is added

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,30 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
+/*
+	Schema::create('art', function($newtable){   //$newtable is a closure parameter for the schema.
+
+		$newtable->increments('id');
+		$newtable->string('artist');
+		$newtable->string('title',500);
+		$newtable->text('description');
+		$newtable->date('created');
+		$newtable->date('exhibition_date');
+		$newtable->timestamps();
+
+	});
+	
+	Schema::table('art', function($newtable){
+
+		$newtable->boolean('alumni');
+		$newtable->dropColumn('exhibition_date');
+	});
+ */
     return view('hello');
 });
+
+
 Route::get('about', function(){
 
 	return "this is about page";
