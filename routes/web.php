679594a28bda335ae57a5d7ba82999bd88c57d0b
Bikram Shrestha <bikramsoftnep@gmail.com>
2018-05-18 15:28:59 +0545

    project restarted

--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,9 @@
 |
 */
 
-Route::get('/', function (){
-
-	Schema::create('art', function($newtable){   //$newtable is a closure parameter for the schema.
-
-		$newtable->increments('id');
-		$newtable->string('artist');
-		$newtable->string('title',500);
-		$newtable->text('description');
-		$newtable->date('created');
-		$newtable->date('exhibition_date');
-		$newtable->timestamps();
-
-	});
-	
-	// Schema::table('art', function($newtable){
-
-	// 	$newtable->boolean('alumni');
-	// 	$newtable->dropColumn('exhibition_date');
-	// });
- 
+Route::get('/', function () {
     return view('hello');
 });
-
-
 Route::get('about', function(){
 
 	return "this is about page";
